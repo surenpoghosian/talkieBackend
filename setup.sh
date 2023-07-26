951e1ebbc38f7a4d13b5af969f7b1178a13908d5
surenpoghosian <surenpoghosyan17@gmail.com>
2023-07-26 18:13:06 +0400

    readme

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ git clone https://github.com/surenpoghosian/talkieBackend
 git clone https://github.com/suno-ai/bark
 cd bark
 pip install bark
-CUDA_VISIBLE_DEVICES=""  python -m bark --text "Hello, my name is Talkie." --history_prompt "v2/ru_speaker_1" --output_filename "example.wav"
+CUDA_VISIBLE_DEVICES=""  python -m bark --text "Hello, my name is Talkie." --history_prompt "v2/en_speaker_1" --output_filename "example.wav"
 cd
 cd talkieBackend
 npm install
